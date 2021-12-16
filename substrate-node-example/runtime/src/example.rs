b8d6be6c543ea3414ee3299f32bb0cb095f96a63
fqmarkvon <fqmarkvon@gmail.com>
2021-12-16 14:44:20 -0500

    add callback

--- substrate-node-example/runtime/src/example.rs
+++ substrate-node-example/runtime/src/example.rs
@@ -5,6 +5,12 @@ use frame_support::{decl_module, decl_storage, dispatch::DispatchResult};
 use frame_system::ensure_root;
 use pallet_chainlink::{CallbackWithParameter, Config as ChainlinkTrait, Event};
 use sp_std::prelude::*;
+//added
+use frame_support::log;
+
+
+
+
 
 type BalanceOf<T> = <<T as pallet_chainlink::Config>::Currency as Currency<
 	<T as frame_system::Config>::AccountId,
@@ -27,22 +33,38 @@ decl_module! {
 
 		#[weight = 0]
 		pub fn send_request(origin, operator: T::AccountId, specid: Vec<u8>) -> DispatchResult {
-			let parameters = ("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD", "path", "RAW.ETH.USD.PRICE", "times", "100000000");
+			log::info!("[send_request]Start the send_request");
+			let parameters = ("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD", "path", "RAW.ETH.USD.PRICE", "times", "100000000"); //original chainlink demo
+			//let parameters = ("pricePair", "LINK/USD"); //from LaurentTrk
+			log::info!("[send_request]Call the send_request");
 			let call: <T as Config>::Callback = Call::callback(vec![]).into();
 
+			//log::info!("[send_request] Calculate the fee");
 			let fee = BalanceOf::<T>::unique_saturated_from(100u32);
+			log::info!("[send_request] the operator is {:?}", operator);
+			//log::info!("[send_request] the specid is {:?}", specid);
+			//log::info!("[send_request] the parameters.encode() is {:?}", parameters.encode());
+			log::info!("[send_request] the fee is {:?}", fee);
 			<pallet_chainlink::Pallet<T>>::initiate_request(origin, operator, specid, 0, parameters.encode(), fee, call.into())?;
+
+			//data version is: 0; [event define] u64
+			// data is: [example.rs]: parameters.encode() [initiate_request]  [36,112,....]; [event define]Vec<u8>
+			// callback: [example.rs]:call.into [initiate_request] "Chainlink.callback".into() [event define]Vec<u8>
+			log::info!("[send_request] Finish the calculation");
 
 			Ok(())
 		}
 
 		#[weight = 0]
 		pub fn callback(origin, result: Vec<u8>) -> DispatchResult {
+			log::info!("[fn.callback]Start the callback");
 			ensure_root(origin)?;
-
+			log::info!("[fn.callback]after check the root");
 			// The result is expected to be a SCALE encoded `i128`
 			let r : i128 = i128::decode(&mut &result[..]).map_err(|_| Error::<T>::DecodingFailed)?;
+			log::info!("[fn.callback] let r");
 			<Result>::put(r);
+			log::info!("[fn.callback] put result");
 
 			Ok(())
 		}
